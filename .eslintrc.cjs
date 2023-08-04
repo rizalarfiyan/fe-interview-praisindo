8056a44e2c0a921a907710b9db1f77e0f3023555
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2023-08-04 22:36:43 +0700

    feat: add eslint plugins and create path alias
    Signed-off-by: Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,35 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
+    'plugin:jsx-a11y/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      alias: {
+        map: [['@/*', './src']],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  plugins: [
+    'react-refresh',
+    '@typescript-eslint',
+    'simple-import-sort',
+    'unused-imports',
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
@@ -19,10 +39,47 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     // prettier
-    'prettier/prettier': [
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    // see: https://www.npmjs.com/package/eslint-plugin-unused-imports
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+    // see: https://www.npmjs.com/package/eslint-plugin-simple-import-sort
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': [
       'error',
-      {},
-      { usePrettierrc: true },
+      {
+        groups: [
+          // ext library & side effect imports
+          ['^@?\\w', '^\\u0000'],
+          // images files
+          ['^.+\\.(gif|jpe?g|tiff?|png|webp|bmp)$'],
+          // css files
+          ['^.+\\.css$'],
+          // relative paths up until 3 level
+          [
+            '^\\./?$',
+            '^\\.(?!/?$)',
+            '^\\.\\./?$',
+            '^\\.\\.(?!/?$)',
+            '^\\.\\./\\.\\./?$',
+            '^\\.\\./\\.\\.(?!/?$)',
+            '^\\.\\./\\.\\./\\.\\./?$',
+            '^\\.\\./\\.\\./\\.\\.(?!/?$)',
+          ],
+          // other that didnt fit in
+          ['^'],
+        ],
+      },
     ],
   },
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import * as path from 'path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, './src'),
+      },
+      {
+        find: '@styles',
+        replacement: path.resolve(__dirname, './src/assets/styles'),
+      },
+    ],
+  },
 })
